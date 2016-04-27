b69175dc4b9fd70bf4f7755192b6a14cca177f1b
BuildTools <unconfigured@null.spigotmc.org>
2016-04-27 17:04:42 -0400

    Added a very basic player, and PlayerMovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿/*
+    Uses basic Up, down, left, and right movement.
+    Movement is based off Horizontal, and Vertical Axis
+  
+        NostroVostro (Trevor LeCroy)
+*/
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] float speed = 3f;
+
+    // Used so that Update can listen for input, and FixedUpdate can move the player 
+    float xMove; 
+    float yMove;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        xMove = Input.GetAxis("Horizontal") * speed;
+        yMove = Input.GetAxis("Vertical") * speed;
+    }
+    
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(xMove, yMove);
+    }
+}
