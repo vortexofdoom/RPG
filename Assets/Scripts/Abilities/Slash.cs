9724c081657b354638c6591479af5e176e0d7fd8
Curtis Fulton <Curtis33210@hotmail.com>
2016-04-30 01:26:00 +1000

    Updated attack square

--- Assets/Scripts/Abilities/Slash.cs
+++ Assets/Scripts/Abilities/Slash.cs
@@ -7,19 +7,31 @@ namespace RPG
     public class Slash : Ability
     {
         private string iconPath = "Sprites/Slash-Ability-Icon";
+        private int attackReach = 2;
+        private int maxTargets = 5;
 
         private Transform charTransform;
 
-        private Vector2 CharPosition { get { return charTransform.position + new Vector3(0, 0.5f); } }
-        private Vector2 Direction { get { return (Vector2)Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1)) - CharPosition; } }
+        private Vector2 CharPosition { get { return charTransform.position + new Vector3(0.5f, 0); } }
+        private Vector2 TargetPosition {
+            get {
+                // Gets the mouse position relative to the world
+                var mousePos = (Vector2)Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
 
-        private RaycastHit2D[] attackHits;
+                // Find the direction of the mouse from the character by finding the difference.
+                // Then normalize it (So it has a length of 1), multiply it by the reach of the attack.
+                // Then offset it to the side of the mouse (The char position goes the opposite direction).
+                return ((mousePos - CharPosition).normalized * 2) + new Vector2(-0.5f, 0);
+            }
+        }
+
+        private Collider2D[] attackHits;
 
         public Slash(Transform transform)
         {
             AbilityName = "Slash";
 
-            attackHits = new RaycastHit2D[5];
+            attackHits = new Collider2D[maxTargets];
 
             charTransform = transform;
 
@@ -32,7 +44,13 @@ namespace RPG
         /// </summary>
         protected override void AbilityImplementation()
         {
-            Debug.Log(Physics2D.BoxCastNonAlloc(CharPosition, new Vector2(1, 1f), 0, Direction.normalized, attackHits, 2, ~(1 << 8)));
+            // This will be changed so the that CharPosition actually corrisponds to a circle around him. This will
+            // Make the attacks more accurate. Atm it is creating a rectangle from the player to the target location. 
+
+            // Using the 'nonAlloc' version will be better for performance as there is no garbage created.
+            // To ignore the player we have to ignore the players layer. This means hit everything except layer 8 (Player layer)
+            // Read up on bit masks if you don't understand bit shifting and how the layers work in unity.
+            Debug.Log(Physics2D.OverlapAreaNonAlloc(CharPosition, TargetPosition, attackHits, ~(1 << 8)));
         }
     }
 }
