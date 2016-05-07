d6734340410c08ae0ca1e35500c42fc2e853f8a1
vortexofdoom <vortexofdoom97@gmail.com>
2016-05-06 21:31:56 -0500

    Added stuff

---
+++ Assets/Scripts/PhysicalAbility.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public abstract class PhysicalAbility : Ability
+{
+	protected override abstract void AbilityImplementation(); //Passing the buck on the Ability Implementation
+
+
+}
--- Assets/Scripts/_old/InputMapping_old.cs
+++ Assets/Scripts/_old/InputMapping_old.cs
@@ -1,54 +1,58 @@
-﻿//using UnityEngine;
-//using System.Collections.Generic;
+﻿using UnityEngine;
+using System.Collections.Generic;
 
-//public class InputMapping : MonoBehaviour {
+public class InputMapping_old : MonoBehaviour
+{
 
-//    private Character character;
-//    [SerializeField]
-//    private AbilityHotbar abilityHotbar;
+	private Character character;
+	[SerializeField]
+	private AbilityHotbar abilityHotbar;
 
-//    private void Start()
-//    {
-//        if (character == null) {
-//            Debug.LogAssertion("Forgot to add movement component in unity inspector");
-//        }
+	private void Start()
+	{
+		if (character == null)
+		{
+			Debug.LogAssertion("Forgot to add character component in unity inspector");
+		}
 
-//        if (abilityHotbar == null) {
-//            Debug.LogAssertion("Forgot to add abilityHotbar in unity inspector");
-//        }
+		if (abilityHotbar == null)
+		{
+			Debug.LogAssertion("Forgot to add abilityHotbar in unity inspector");
+		}
 
-//		character = GetComponent<Character>();
-//	}
+		character = GetComponent<Character>();
+	}
 
-//    private void Update()
-//    {
-//        UpdateVelocity();
-//        UpdateAttacks();
-//    }
+	private void Update()
+	{
+		UpdateVelocity();
+		UpdateAttacks();
+	}
 
-//    private void UpdateAttacks()
-//    {
-//        if (Input.GetMouseButtonDown(0)) {
-//            abilityHotbar.UseAbility(0);
-//        }
-//    }
+	private void UpdateAttacks()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			abilityHotbar.UseAbility(0);
+		}
+	}
 
-//    /// <summary>
-//    /// Updates the players velocity based on inputs.
-//    /// </summary>
-//    private void UpdateVelocity()
-//    {
-//        var velocity = new Vector2();
+	/// <summary>
+	/// Updates the players velocity based on inputs.
+	/// </summary>
+	private void UpdateVelocity()
+	{
+		var velocity = new Vector2();
 
-//        // Will probably consider using ".GetAxisRaw" instead? This has a smoothing effect
-//        // Which means you don't stop straight away
-//        velocity.x = Input.GetAxis("Horizontal");
-//        velocity.y = Input.GetAxis("Vertical");
+		// Will probably consider using ".GetAxisRaw" instead? This has a smoothing effect
+		// Which means you don't stop straight away
+		velocity.x = Input.GetAxis("Horizontal");
+		velocity.y = Input.GetAxis("Vertical");
 
-//        // Stops the velocity being dependent on framerate
-//        velocity *= Time.deltaTime;
+		// Stops the velocity being dependent on framerate
+		velocity *= Time.deltaTime;
 
-//        character.AddVelocity(velocity);
+		character.AddVelocity(velocity);
 
-//    }
-//}
+	}
+}
